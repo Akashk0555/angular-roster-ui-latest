9fe17fc80899b3f730bad3561b975dd54cac8b06
Akash Kumar <akashkumar@Akashs-MacBook-Air.local>
2025-08-15 21:29:23 +0530

    Optimized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import { AppRoutingModule } from './app-routing.module';
+import { App } from './app.component';
 import { Dashboard } from './components/dashboard/dashboard';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddComment } from './components/add-comment/add-comment';
 import { SideBar } from './components/side-bar/side-bar';
 import { TopBar } from './components/top-bar/top-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShowComponent } from './components/show/show.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +18,16 @@ import { TopBar } from './components/top-bar/top-bar';
     Dashboard,
     AddComment,
     SideBar,
-    TopBar
+    TopBar,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/components/show/show.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.component.html',
+  styleUrl: './show.component.scss',
+  standalone:false
+})
+export class ShowComponent {
+
+}
